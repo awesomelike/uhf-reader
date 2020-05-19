c387745276c509788a173f492bd1b68535d9075a
unknown <mr.azik98@mail.ru>
2020-05-19 15:05:30 +0500

    added close and error events

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ app.listen(3000, () => {
     let bytes = Buffer.from([0x04, 0xff, 0x21, 0x19, 0x95], "ascii");
     client.write(bytes);
 
-    client.on('data', function(data) {
+    client.on('data', (data) => {
       console.log('UHF reader response: ');
       var buffer = Buffer.from(data, "ascii");
       console.log(buffer);
+    });
+    
+    client.on('close', () => {
+      console.log('UHF Reader connection closed!');
+    });
+
+    client.on('error', (error) => {
+      console.log(error);
     });
     
   });
