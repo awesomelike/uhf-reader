d2d8703789f4fca75f391f1751cf8df26887b6c3
awesomelike <abdullaev.dev@gmail.com>
2020-09-19 18:07:27 +0500

    tabs removed

--- listener.js
+++ listener.js
@@ -17,89 +17,89 @@ const io = socketIO(server);
 require('dotenv').config();
 
 server.listen(port, async () => {
-	try {
-		const response = await axios.post(`${apiUrl}/auth/login`, {
-			username: process.env.API_LOGIN,
-			password: process.env.API_PWD,
-		});
-		if (response.status === 401 || response.status === 404) {
-			return console.log(response.data);
-		}
+  try {
+    const response = await axios.post(`${apiUrl}/auth/login`, {
+      username: process.env.API_LOGIN,
+      password: process.env.API_PWD,
+    });
+    if (response.status === 401 || response.status === 404) {
+      return console.log(response.data);
+    }
 
-		axios.defaults.headers.post[
-			'Authorization'
-		] = `Bearer ${response.data.token}`;
+    axios.defaults.headers.post[
+      'Authorization'
+    ] = `Bearer ${response.data.token}`;
 
-		console.log('Logged in successfully!');
-		console.log(`Listener service started on ${port}`);
+    console.log('Logged in successfully!');
+    console.log(`Listener service started on ${port}`);
 
-		const reader = new net.Socket();
+    const reader = new net.Socket();
 
-		reader.setEncoding('ascii');
+    reader.setEncoding('ascii');
 
-		reader
-			.connect(process.env.READER_PORT, process.env.READER_IP, () => {})
-			.on('error', (error) => console.log(error));
+    reader
+      .connect(process.env.READER_PORT, process.env.READER_IP, () => {})
+      .on('error', (error) => console.log(error));
 
-		reader.on('connect', (data) => {
-			console.log('UHF Reader connected');
+    reader.on('connect', (data) => {
+      console.log('UHF Reader connected');
 
-			// Set to Answer mode
-			reader.write(ANSWER_MODE);
+      // Set to Answer mode
+      reader.write(ANSWER_MODE);
 
-			io.on('connection', (socket) => {
-				console.log('Client connected!');
+      io.on('connection', (socket) => {
+        console.log('Client connected!');
         
         const SET = new Set();
 
-				let interval;
-				socket.on('bookId', (bookId) => {
-					console.log(`Client sent bookId=${bookId}`);
-					
-					// Send inventory command
-					interval = setInterval(() => {
-						reader.write(INVENTORY);
-					}, 100);
-					
-					notifier.notify({
-						title: 'The reader is ready to listen',
-						message: `Received book id: ${bookId}`,
-					});
+        let interval;
+        socket.on('bookId', (bookId) => {
+          console.log(`Client sent bookId=${bookId}`);
+          
+          // Send inventory command
+          interval = setInterval(() => {
+            reader.write(INVENTORY);
+          }, 100);
+          
+          notifier.notify({
+            title: 'The reader is ready to listen',
+            message: `Received book id: ${bookId}`,
+          });
 
           reader
-						.on('data', (data) => {
-							const tags = getTags(data);
-							if (Array.isArray(tags)) {
-								tags.forEach((tag) => {
-									if (!SET.has(tag)) {
+            .on('data', (data) => {
+              const tags = getTags(data);
+              if (Array.isArray(tags)) {
+                tags.forEach((tag) => {
+                  if (!SET.has(tag)) {
                     SET.add(tag);
                     socket.emit('bookItem', { bookId, rfidTag: tag });
-										console.log('hash: ', tag);
-									}
-								});
-							}
-						})
-						.on('error', (err) => console.log(err));
-				});
-				socket.on('rfidTag', () => {
+                    console.log('hash: ', tag);
+                  }
+                });
+              }
+            })
+            .on('error', (err) => console.log(err));
+        });
+        socket.on('rfidTag', () => {
           const message = 'RFID Lookup request received';
-					console.log(message);
+          console.log(message);
 
-					// Send inventory command
-					interval = setInterval(() => {
-						reader.write(INVENTORY);
-					}, 100);
+          // Send inventory command
+          interval = setInterval(() => {
+            reader.write(INVENTORY);
+          }, 100);
 
-					notifier.notify({
-						title: message,
-						message,
-					});
+          notifier.notify({
+            title: message,
+            message,
+          });
 
-					reader.on('data', async (data) => {
+          reader.on('data', async (data) => {
             const tags = getTags(data);
-							if (Array.isArray(tags)) {
-								tags.forEach(async (tag) => {
-									if (!SET.has(tag)) {
+              if (Array.isArray(tags)) {
+                tags.forEach(async (tag) => {
+                  if (!SET.has(tag)) {
                     SET.add(tag);
                     console.log('hash: ', tag);
                     try {
@@ -110,28 +110,28 @@ server.listen(port, async () => {
                     } catch (error) {
                       console.log(error);
                     }
-									}
-								});
-							}
-					});
-				});
+                  }
+                });
+              }
+          });
+        });
 
         socket.on('inventory', () => {
-					const message = 'Inventory has started!';
-					
-					// Send inventory command
-					interval = setInterval(() => {
-						reader.write(INVENTORY);
-					}, 100);
-					
-					console.log(message);
-					
-					notifier.notify({
-						title: message,
-						message,
+          const message = 'Inventory has started!';
+          
+          // Send inventory command
+          interval = setInterval(() => {
+            reader.write(INVENTORY);
+          }, 100);
+          
+          console.log(message);
+          
+          notifier.notify({
+            title: message,
+            message,
           });
-					
-					reader.on('data', (data) => {
+          
+          reader.on('data', (data) => {
             const tags = getTags(data);
             if (Array.isArray(tags)) {
               tags.forEach((tag) => {
@@ -143,44 +143,44 @@ server.listen(port, async () => {
             }
           });
           setTimeout(() => {
-						clearInterval(interval);
+            clearInterval(interval);
             socket.emit('inventoryResults', { items: Array.from(SET) });
           }, 10000);
         });
 
-				socket.on('delete', (data) => {
-					console.log('Delete request received, hash:', data.rfidTag);
-					SET.delete(data.rfidTag);
-				});
+        socket.on('delete', (data) => {
+          console.log('Delete request received, hash:', data.rfidTag);
+          SET.delete(data.rfidTag);
+        });
 
-				socket.on('disconnect', () => {
-					console.log('Client disconnected');
-					reader.removeAllListeners();
+        socket.on('disconnect', () => {
+          console.log('Client disconnected');
+          reader.removeAllListeners();
           socket.removeAllListeners();
           if (interval) clearInterval(interval);
           SET.clear();
-				});
-			});
+        });
+      });
 
-			reader.on('error', (error) => {
-				console.log('UHF Reader error!', error);
-				process.exit(-1);
-			});
-			reader.on('timeout', () => {
-				console.log('Reader connection timeout!');
-				process.exit(-1);
-			})
-			reader.on('close', (error) => {
-				console.log('Client destroyed:', reader.destroyed);
-				console.log('UHF Reader connection closed!', error);
-				process.exit(-1);
-			});
-		});
-	} catch (error) {
-		console.error(
-			'Error ocurred! Check if server is up or check your login/password...'
-		);
-		console.log(error);
-		process.exit(-1);
-	}
+      reader.on('error', (error) => {
+        console.log('UHF Reader error!', error);
+        process.exit(-1);
+      });
+      reader.on('timeout', () => {
+        console.log('Reader connection timeout!');
+        process.exit(-1);
+      })
+      reader.on('close', (error) => {
+        console.log('Client destroyed:', reader.destroyed);
+        console.log('UHF Reader connection closed!', error);
+        process.exit(-1);
+      });
+    });
+  } catch (error) {
+    console.error(
+      'Error ocurred! Check if server is up or check your login/password...'
+    );
+    console.log(error);
+    process.exit(-1);
+  }
 });
